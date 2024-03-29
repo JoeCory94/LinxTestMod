480c6b9888678e7535f4be7aaaff9fb09361d581
Joe Cory <joe@chair8media.com>
2024-03-29 21:01:19 +0000

    Initial commit

---
+++ src/main/java/net/linx/linxtestmod/LinxTestMod.java
@@ -0,0 +1,17 @@
+package net.linx.linxtestmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LinxTestMod implements ModInitializer {
+	public static final String MOD_ID = "linxtestmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger("linxtestmod");
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/linx/linxtestmod/LinxTestModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.linx.linxtestmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LinxTestModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
